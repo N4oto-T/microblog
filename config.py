6ca1b4584b54c72f0ff337dd30154ecbf0206acc
N4oto-T <h13.only57@gmail.com>
2024-01-20 19:17:14 +0900

    error handler

--- config.py
+++ config.py
@@ -3,8 +3,15 @@ import os
 basedir = os.path.abspath(os.path.dirname(__name__))
 
 
-class Config:
+class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-neve-guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
+
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = os.environ.get("MAIL_PORT" or 25)
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    ADMINS = ["sample.example.com"]
