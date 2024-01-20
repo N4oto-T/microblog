b7857a87d70986bbfad1ff982ea05623af0fcc9f
N4oto-T <h13.only57@gmail.com>
2024-01-20 19:28:09 +0900

    Fixing the duplicate username bug

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 import sqlalchemy as sa
 from flask_wtf import FlaskForm
+from flask_wtf.form import _Auto
 from wtforms import BooleanField, PasswordField, StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 
@@ -38,3 +39,22 @@ class EditProfileForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
     submit = SubmitField("Submit")
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
+
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        # Formに入力された変更後のusernameと、現在のusernameが異なる場合にだけdbチェック走る
+        if username.data != self.original_username:
+            user = db.session.scalar(
+                sa.select(User).where(User.username == self.username.data)
+            )
+            if user is not None:
+                raise ValidationError("Please use a different username")
