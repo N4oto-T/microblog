b1d070dbd3995c99042f72ade28bb0f6242d6ae0
N4oto-T <h13.only57@gmail.com>
2024-01-22 20:53:32 +0900

    page navigation

--- app/routes.py
+++ app/routes.py
@@ -38,6 +38,17 @@ def index():
         per_page=app.config["POSTS_PER_PAGE"],
         error_out=False,
     )
+    next_url = url_for("index", page=posts.prev_num) if posts.has_next else None
+    prev_url = url_for("index", page=posts.prev_num) if posts.has_prev else None
+
+    return render_template(
+        "index.html",
+        title="Home",
+        form=form,
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
 
     return render_template("index.html", title="Home", posts=posts.items, form=form)
 
@@ -166,5 +177,13 @@ def explore():
     posts = db.paginate(
         query, page=page, per_page=app.config["POSTS_PER_PAGE"], error_out=False
     )
+    next_url = url_for("explore", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("explore", page=posts.prev_num) if posts.has_prev else None
     # exploreとindexページは似た構造になるのでindexを再利用する。しかし、blogを書くformは表示させたくないのでform引数はなし。
-    return render_template("index.html", title="Explore", posts=posts.items)
+    return render_template(
+        "index.html",
+        title="Explore",
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
